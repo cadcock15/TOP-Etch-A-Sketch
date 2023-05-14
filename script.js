1617eef738e10cad5138a1c5f71baaeced843006
cadcock15 <5466552+cadcock15@users.noreply.github.com>
2023-05-14 00:00:56 -0700

    Add Grid

--- script.js
+++ script.js
@@ -3,3 +3,23 @@
  * Etch A Sketch
  * @author: Christopher Adcock
  */
+
+
+createGrid();
+
+function createGrid() {
+    const gridContainer = document.querySelector('#gridContainer');
+    
+    for(i=1;i<=16;i++) {
+        let gridRowDiv = document.createElement("div");
+        gridRowDiv.id = "row_" + i;
+        gridRowDiv.classList.add("rowFlexbox");
+        gridContainer.appendChild(gridRowDiv);
+        for(j=1;j<=16;j++) {
+            let gridColDiv = document.createElement("div");
+            gridColDiv.id = "cell_r" + i + "c" + j;
+            gridColDiv.classList.add("cells");
+            gridRowDiv.appendChild(gridColDiv);
+        }
+    }
+}
